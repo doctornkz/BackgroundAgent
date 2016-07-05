542da1e8fa30089c6e6ee1f1fcd42b77cd53ab05
doctornkz <doctornkz@yandex.ru>
2016-07-06 02:27:43 +0300

    some experiments

--- app/src/main/java/com/local/doctor/backgroundagent/Agent.java
+++ app/src/main/java/com/local/doctor/backgroundagent/Agent.java
@@ -1,40 +1,45 @@
 package com.local.doctor.backgroundagent;
 
+import android.app.IntentService;
+
+import android.content.Intent;
+import android.util.Log;
+
 /**
  * Created by doctor on 05.07.16.
  */
 
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import android.util.Log;
-import android.widget.Toast;
-
-public class Agent extends Service{
-    String tag="Agent";
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Toast.makeText(this, "Service created...", Toast.LENGTH_LONG).show();
-        Log.i(tag, "Service created...");
+public class Agent extends IntentService {
+    private static final String TAG = "zxcvbnm11"; /* define log tag*/
+    // Must create a default constructor
+    public Agent() {
+        // Used to name the worker thread, important only for debugging.
+        super("zxcvbnm");
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Service started...", Toast.LENGTH_LONG).show();
-        super.onStartCommand(intent, flags ,startId);
-        Log.i(tag, "Service started...");
-        return START_NOT_STICKY;
-    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "Service destroyed...", Toast.LENGTH_LONG).show();
+    public void onCreate()  {
+        int max = 200;
+        super.onCreate(); // if you override onCreate(), make sure to call super().
+        // If a Context object is needed, call getApplicationContext() here.
+
+        // Use Activity method to create a file in the writeable directory
+        for (int i=0; i < max; i++) {
+
+            try {
+                Log.i(TAG, "");
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Log.e(TAG, "");
+            }
+        }
     }
 
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    protected void onHandleIntent(Intent intent) {
+        // This describes what will happen when service is triggered
     }
 }
--- app/src/main/java/com/local/doctor/backgroundagent/MainActivity.java
+++ app/src/main/java/com/local/doctor/backgroundagent/MainActivity.java
@@ -1,7 +1,31 @@
 package com.local.doctor.backgroundagent;
 
-/**
- * Created by doctor on 05.07.16.
- */
-public class MainActivity {
-}
+import android.app.Activity;
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class MainActivity  extends Activity {
+        private static final String TAG = ""; /* define log tag*/
+        // Call `launchTestService()` in the activity
+        // to startup the service
+
+        public void launchTestService() {
+
+            // Construct our Intent specifying the Service
+            Intent i = new Intent(this, Agent.class);
+            // Add extras to the bundle
+            i.putExtra("foo", "bar");
+            // Start the service
+            startService(i);
+
+
+            Log.e(TAG, "");
+
+        }
+
+
+    }
+
+
